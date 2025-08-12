fd3d6b9e0ebec80bbee515ed5dfe6ae2a27988f6
23hr03 <landaetahectorrenier@gmail.com>
2025-08-12 19:43:39 -0300

    add: layout implementado

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () =>{
+    return (
+        <footer>
+            <p>Este sitio web fue creado por hector</p>
+        </footer>
+    )
+}
+
+export { Footer }
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+    return (
+        <header >
+            <nav>
+                <img src="https://i.pinimg.com/736x/d9/d0/3b/d9d03b4b8d515fc118835bb29ddff604.jpg" alt="logo" />
+                <ul>
+                    <li><a href="/"></a>Inicio</li>
+                    <li><a href="dashboard">Dashboard</a></li>
+                    <li><a href="login">Login</a></li>
+                    <li><a href="registrate">Registrate</a></li>
+                    <button>Cerrar sesion</button>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export {Header}
