930f858c5762ecf4fa8be40587f97bd216c81527
23hr03 <landaetahectorrenier@gmail.com>
2025-08-12 21:05:37 -0300

    add: instalacion de react-router-dom y agregar todas las pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-const Header = () => {
+function Header() {
     return (
-        <header >
-            <nav>
+        <header className="header">
+            <nav >
                 <img src="https://i.pinimg.com/736x/d9/d0/3b/d9d03b4b8d515fc118835bb29ddff604.jpg" alt="logo" />
                 <ul>
                     <li><a href="/"></a>Inicio</li>
---
+++ src/views/Register.jsx
@@ -0,0 +1,11 @@
+import { Layout } from "../components/Layout";
+
+const  Register = () => {
+    return (
+        <Layout background = "blue">
+            <h1>Hola desde registrate</h1>
+        </Layout>
+    )
+}
+
+export {  Register }
