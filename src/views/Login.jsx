d05fa656bd2f9553c415d1ca9fe5bb762bc67632
23hr03 <landaetahectorrenier@gmail.com>
2025-08-12 21:31:48 -0300

    add: Creacion de la RouterApp con todas las rutas

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -2,7 +2,7 @@ import { Layout } from "../components/Layout";
 
 const Login = () => {
     return (
-        <Layout background = "blue">
+        <Layout>
             <h1>Hola desde Login</h1>
         </Layout>
     )
--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -2,7 +2,7 @@ import { Layout } from "../components/Layout";
 
 const NotFound = () => {
     return (
-        <Layout background = "blue">
+        <Layout>
             <h1>Hola desde NotFound</h1>
         </Layout>
     )
