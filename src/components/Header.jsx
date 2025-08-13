a4e2a7e37fccc9916ac07e94b6f6aa2c98b709c8
23hr03 <landaetahectorrenier@gmail.com>
2025-08-12 21:38:47 -0300

    add: Cambios en el componets Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
-function Header() {
+import { Link } from "react-router-dom"
+
+
+const Header =()=> {
     return (
         <header className="header">
             <nav >
                 <img src="https://i.pinimg.com/736x/d9/d0/3b/d9d03b4b8d515fc118835bb29ddff604.jpg" alt="logo" />
                 <ul>
-                    <li><a href="/"></a>Inicio</li>
-                    <li><a href="dashboard">Dashboard</a></li>
-                    <li><a href="login">Login</a></li>
-                    <li><a href="registrate">Registrate</a></li>
+                    <li><Link to="/"></Link>Inicio</li>
+                    <li><Link to="dashboard">Dashboard</Link></li>
+                    <li><Link to="login">Login</Link></li>
+                    <li><Link to="registrate">Registrate</Link></li>
                     <button>Cerrar sesion</button>
                 </ul>
             </nav>
