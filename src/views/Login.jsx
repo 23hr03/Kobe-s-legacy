761cd73830480f66dad13720f60d13129daa4822
23hr03 <landaetahectorrenier@gmail.com>
2025-08-13 23:36:32 -0300

    add: agregue una imagen de fondo y el formulario de login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -3,7 +3,26 @@ import { Layout } from "../components/Layout";
 const Login = () => {
     return (
         <Layout>
-            <h1>Hola desde Login</h1>
+            <h1>Inicia session</h1>
+
+            <section>
+                <h2> Bienvendo, el legado de kobe te espera </h2>
+                <form>
+                    <div>
+                        <label>Nombre de usuario</label>
+                        <input type="text" name="nombre" />
+                    </div>
+                    <div>
+                        <label>Correo electronico</label>
+                        <input type="email" name="email" />
+                    </div>
+                    <div>
+                        <label>Contraseña</label>
+                        <input type="password" name="password"/>
+                    </div>
+                    <button>iniciar session</button>
+                </form>
+            </section>
         </Layout>
     )
 }
