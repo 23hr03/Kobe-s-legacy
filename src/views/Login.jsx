e473c9ccd0f61fb68715972898daeb4c3132b1c1
23hr03 <landaetahectorrenier@gmail.com>
2025-08-16 20:06:31 -0300

    add: login estilos completos

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,28 +1,37 @@
 import { Layout } from "../components/Layout";
+import "../styles/views/Login.css"
 
 const Login = () => {
     return (
         <Layout>
             <h1>Inicia session</h1>
 
-            <section>
-                <h2> Bienvendo, el legado de kobe te espera </h2>
-                <form>
+            <section className="login">
+                <h2 > Bienvendo, el legado de kobe te espera </h2>
+                <form className="formulario">
                     <div>
-                        <label>Nombre de usuario</label>
+                        <label>Nombre de usuario:</label>
                         <input type="text" name="nombre" />
                     </div>
                     <div>
-                        <label>Correo electronico</label>
+                        <label>Correo electronico:</label>
                         <input type="email" name="email" />
                     </div>
                     <div>
-                        <label>Contraseña</label>
+                        <label>Contraseña:</label>
                         <input type="password" name="password"/>
                     </div>
-                    <button>iniciar session</button>
+                    <div className="botones">
+                        <button className="enviar">Registrar</button>
+                        <button className="enviar">Iniciar session</button>
+                    </div>
+                    
                 </form>
             </section>
+
+            <main>
+                <p> Inicia session para que puedas difrutar de nuestra tienda online con los mejores productos de la NBA🏀</p>
+            </main>
         </Layout>
     )
 }
