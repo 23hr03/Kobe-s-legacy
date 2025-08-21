a7be34424b632a72fd98eb7169fd89ed6b3ce447
23hr03 <landaetahectorrenier@gmail.com>
2025-08-21 20:00:10 -0300

    add: implementacion para agregar productos

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -3,47 +3,92 @@ import { Layout } from "../components/Layout";
 import "../styles/views/Dashboard.css"
 
 const Dashboard = () => {
-  const[name, setName] = useState()
-  const[price, setPrice] = useState()
-  const[description, setDescription] = useState()
+  const[name, setName] = useState("")
+  const[price, setPrice] = useState("")
+  const[description, setDescription] = useState("")
+  const[product, setProduct] = useState(null)
+  const[error, setError] = useState(null)
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = async (e) =>{
     e.preventDefault()
+
+    setError(null)
+
+    if (!name || !price || !description){
+      setError("Debes completar todos los campos")
+      return
+    }
 
     const newProduct ={
       id: crypto.randomUUID(),
-      name: name,
+      title: name,
       price: price,
-      description: description
+      description: description,
+      category: "",
+      image: ""
      }
+
+    const response =  await fetch("https://fakestoreapi.com/products", 
+      {method: "POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newProduct)
+    })
+
+    const data = await response.json()
+
+     setProduct(data) 
+
+     setName("")
+     setPrice("")
+     setDescription("")
 
   }
     return (
         <Layout>
             <h1>Panel de administracion</h1>
-            
-            <form  onSubmit={handleSubmit}>
+            <section>
+              <form  onSubmit={handleSubmit}>
 
-                <label> Nombre del producto:
-                  <input type="text" name="nombre" onChange={(e) => setName(e.target.value)} />
-                </label>
-            
-                <label> Precio del producto:
-                  <input type="text" name="price" onChange={(e) => setName(e.target.value)} />
-                </label>
-    
-                <label> Descripcion :
-                  <input type="text" name="description" onChange={(e) => setName(e.target.value)} />
-                </label>
+                  <label> Nombre del producto:
+                    <input type="text" name="nombre" onChange={(e) => setName(e.target.value)} value={name} />
+                  </label>
 
-              <div>
-                <button className="btn"> Crear producto</button>
-              </div>
+                  <label> Precio del producto:
+                    <input type="text" name="price" onChange={(e) => setPrice(e.target.value)} value={price} />
+                  </label>
+      
+                  <label> Descripcion :
+                    <textarea name="description"  rows="4" onChange={(e) => setDescription(e.target.value)} value={description}> </textarea>
+                  </label>
 
-              
+                <div>
+                  <button className="btn"> Guardar producto</button>
+                </div> 
 
-            </form>
-        </Layout>
+              </form>
+              {
+
+              error && <p className="texto-error">{error}</p>
+              } 
+            </section>
+
+            <section className="new-product">
+            {
+                  product && <div>
+                    <h2>{product.title}</h2>
+                    <img src={product.image} alt={`imagen de ${product.title}`}/>
+                    <p>{product.price}</p>
+                    <p>{product.description}</p>
+                    <p><strong>{product.category}</strong> </p>
+                  </div>
+                }   
+                
+            </section>
+
+ 
+   </Layout>
     )
 }
 
