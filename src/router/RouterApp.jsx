74d71dff18570afac1cbce45bc708eaeb575b581
23hr03 <landaetahectorrenier@gmail.com>
2025-08-25 20:00:23 -0300

    cambios

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -10,7 +10,7 @@ const RouterApp = () => {
     return(
         <BrowserRouter>
         <Routes>
-            <Route path="/login" element ={<Login /> }  />
+            <Route path="/login" element ={<Login/> }  />
             <Route path="/home" element = {<Home/>}/>
             <Route path="/dashboard" element = {<PrivateRoute><Dashboard/></PrivateRoute>}/>
             <Route path="/register" element = {<Register/>}/>
