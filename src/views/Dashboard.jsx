d082f7006b1101ee2bc28cbf49ad94b1b1f40319
23hr03 <landaetahectorrenier@gmail.com>
2025-08-24 23:29:45 -0300

    creacion de la pagina resposive

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -6,6 +6,8 @@ const Dashboard = () => {
   const[name, setName] = useState("")
   const[price, setPrice] = useState("")
   const[description, setDescription] = useState("")
+  const[image, setImage] = useState("")
+  const[category, setCategory] = useState("")
   const[product, setProduct] = useState(null)
   const[error, setError] = useState(null)
 
@@ -24,8 +26,8 @@ const Dashboard = () => {
       title: name,
       price: price,
       description: description,
-      category: "",
-      image: ""
+      category: category,
+      image: image
      }
 
     const response =  await fetch("https://fakestoreapi.com/products", 
@@ -43,12 +45,16 @@ const Dashboard = () => {
      setName("")
      setPrice("")
      setDescription("")
+     setCategory("")
+     setImage("")
 
   }
     return (
         <Layout>
-            <h1>Panel de administracion</h1>
-            <section>
+            <h1>Agrega nuevos productos</h1>
+
+            
+            <section className="container">
               <form  onSubmit={handleSubmit}>
 
                   <label> Nombre del producto:
@@ -56,36 +62,45 @@ const Dashboard = () => {
                   </label>
 
                   <label> Precio del producto:
-                    <input type="text" name="price" onChange={(e) => setPrice(e.target.value)} value={price} />
+                    <input type="number" name="price" onChange={(e) => setPrice(e.target.value)} value={price} />
                   </label>
       
+                  <label> Categoria:
+                    <input type="text" name="category" onChange={(e) => setCategory(e.target.value)} value={category} />
+                  </label>
+      
+
+                  <label> URL de la imagen:
+                    <input type="text" name="image" onChange={(e) => setImage(e.target.value)} value={image} />
+                  </label>
+
                   <label> Descripcion :
                     <textarea name="description"  rows="4" onChange={(e) => setDescription(e.target.value)} value={description}> </textarea>
                   </label>
-
                 <div>
                   <button className="btn"> Guardar producto</button>
                 </div> 
 
               </form>
+              <section className="new-product">
+              {
+                    product && <div>
+                      <h2>{product.title}</h2>
+                      <img src={product.image} alt={`imagen de ${product.title}`}/>
+                      <p><strong>${product.price}</strong></p>
+                      <p>{product.category}</p>
+                      <p>{product.description}</p>
+                    </div>
+                  }   
+
+              </section>
               {
 
               error && <p className="texto-error">{error}</p>
               } 
             </section>
 
-            <section className="new-product">
-            {
-                  product && <div>
-                    <h2>{product.title}</h2>
-                    <img src={product.image} alt={`imagen de ${product.title}`}/>
-                    <p>{product.price}</p>
-                    <p>{product.description}</p>
-                    <p><strong>{product.category}</strong> </p>
-                  </div>
-                }   
-                
-            </section>
+           
 
  
    </Layout>
