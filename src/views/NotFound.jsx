13c8e5d3afd30d21b9fe98fd8a818551c4187510
23hr03 <landaetahectorrenier@gmail.com>
2025-08-25 22:47:36 -0300

    cambios notfound

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,19 +1,22 @@
 import { Layout } from "../components/Layout";
+import { Link } from "react-router-dom";
 import "../styles/views/NotFound.css";
 
 const NotFound = () => {
-    return (
-      <Layout>
-        <div className="notfound-card">
-          <h1 className="notfound-title">404</h1>
-          <h2 className="notfound-subtitle">Página no encontrada</h2>
-          <p className="notfound-text">
-            Lo sentimos, la página que buscas no existe o fue movida.
-          </p>
-          <a href="/" className="notfound-button">Volver al inicio</a>
-        </div>
-      </Layout>
-    );
-  };
-  
-  export { NotFound };
+  return (
+    <Layout>
+      <div className="notfound-card">
+        <h1 className="notfound-title">404</h1>
+        <h2 className="notfound-subtitle">Página no encontrada</h2>
+        <p className="notfound-text">
+          Lo sentimos, la página que buscas no existe o fue movida.
+        </p>
+        <Link to="/login" className="notfound-button">
+          Ir al Login
+        </Link>
+      </div>
+    </Layout>
+  );
+};
+
+export { NotFound };
