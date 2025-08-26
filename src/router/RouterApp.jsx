1744644c28b3e8df96f53977b31f2c23d590244f
23hr03 <landaetahectorrenier@gmail.com>
2025-08-25 21:59:35 -0300

    cambios en el routerapp

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -1,23 +1,26 @@
-import {HashRouter, Routes,Route} from "react-router-dom"
-import { Home } from "../views/Home"
-import { Dashboard } from "../views/Dashboard"
-import { Login } from "../views/Login"
-import { Register } from "../views/Register"
-import { NotFound } from "../views/NotFound"
-import { PrivateRoute } from "../components/PrivateRoute"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Home } from "../views/Home";
+import { Dashboard } from "../views/Dashboard";
+import { Login } from "../views/Login";
+import { Register } from "../views/Register";
+import { NotFound } from "../views/NotFound";
+import { PrivateRoute } from "../components/PrivateRoute";
 
-const RouterApp = () => {
-    return(
-        <HashRouter>
-            <Routes>
-                <Route path="/" element={<Register />} />  {/* página inicial */}
-                <Route path="/home" element={<Home />} />
-                <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </HashRouter>
-    )
-}
-export {RouterApp}
+const RouterApp = () => (
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Routes>
+      {/* Redirige la raíz a /register */}
+      <Route path="/" element={<Navigate to="/register" />} />
+
+      <Route path="/home" element={<Home />} />
+      <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      
+      {/* Para rutas no existentes */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export { RouterApp };
