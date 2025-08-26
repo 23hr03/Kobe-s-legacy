673f0898ae4160294b809dd9235ce032ea421cfb
23hr03 <landaetahectorrenier@gmail.com>
2025-08-25 22:17:58 -0300

    cambios

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -1,22 +1,21 @@
-import {BrowserRouter, Routes,Route} from "react-router-dom"
-import { Home } from "../views/Home"
-import { Dashboard } from "../views/Dashboard"
-import { Login } from "../views/Login"
-import { Register } from "../views/Register"
-import { NotFound } from "../views/NotFound"
-import { PrivateRoute } from "../components/PrivateRoute"
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { Register } from "../views/Register";
+import { Login } from "../views/Login";
+import { Dashboard } from "../views/Dashboard";
+import { PrivateRoute } from "../components/PrivateRoute";
+import { Home } from "../views/Home";
+import { NotFound } from "../views/NotFound";
 
-const RouterApp = () => {
-    return(
-        <BrowserRouter>
-        <Routes>
-            <Route path="/login" element ={<Login/> }  />
-            <Route path="/home" element = {<Home/>}/>
-            <Route path="/dashboard" element = {<PrivateRoute><Dashboard/></PrivateRoute>}/>
-            <Route path="/register" element = {<Register/>}/>
-            <Route path="*" element = {<NotFound/>}/>
-        </Routes>
-        </BrowserRouter>
-    )
-}
-export {RouterApp}
+const RouterApp = () => (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </HashRouter>
+);
+
+export { RouterApp };
