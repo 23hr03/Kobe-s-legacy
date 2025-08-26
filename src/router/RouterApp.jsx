0a85837fc514e86f065952559962b29b2e1b2497
23hr03 <landaetahectorrenier@gmail.com>
2025-08-25 22:08:10 -0300

    version anterior

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -1,25 +1,22 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Home } from "../views/Home";
-import { Dashboard } from "../views/Dashboard";
-import { Login } from "../views/Login";
-import { Register } from "../views/Register";
-import { NotFound } from "../views/NotFound";
-import { PrivateRoute } from "../components/PrivateRoute";
+import {BrowserRouter, Routes,Route} from "react-router-dom"
+import { Home } from "../views/Home"
+import { Dashboard } from "../views/Dashboard"
+import { Login } from "../views/Login"
+import { Register } from "../views/Register"
+import { NotFound } from "../views/NotFound"
+import { PrivateRoute } from "../components/PrivateRoute"
 
-const RouterApp = () => (
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <Routes>
-      {/* Redirige la raíz a /register */}
-      <Route path="/" element={<Navigate to="/register" replace />} />
-
-      <Route path="/home" element={<Home />} />
-      <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
-);
-
-export { RouterApp };
-
+const RouterApp = () => {
+    return(
+        <BrowserRouter>
+        <Routes>
+            <Route path="/login" element ={<Login/> }  />
+            <Route path="/home" element = {<Home/>}/>
+            <Route path="/dashboard" element = {<PrivateRoute><Dashboard/></PrivateRoute>}/>
+            <Route path="/register" element = {<Register/>}/>
+            <Route path="*" element = {<NotFound/>}/>
+        </Routes>
+        </BrowserRouter>
+    )
+}
+export {RouterApp}
