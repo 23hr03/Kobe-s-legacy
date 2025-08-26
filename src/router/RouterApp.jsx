879b966a06cf9d09dd8d255278256b47d969bbb6
23hr03 <landaetahectorrenier@gmail.com>
2025-08-25 21:53:12 -0300

    Actualizacion de las rutas

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes,Route} from "react-router-dom"
+import {HashRouter, Routes,Route} from "react-router-dom"
 import { Home } from "../views/Home"
 import { Dashboard } from "../views/Dashboard"
 import { Login } from "../views/Login"
@@ -8,15 +8,16 @@ import { PrivateRoute } from "../components/PrivateRoute"
 
 const RouterApp = () => {
     return(
-        <BrowserRouter>
-        <Routes>
-            <Route path="/login" element ={<Login/> }  />
-            <Route path="/home" element = {<Home/>}/>
-            <Route path="/dashboard" element = {<PrivateRoute><Dashboard/></PrivateRoute>}/>
-            <Route path="/register" element = {<Register/>}/>
-            <Route path="*" element = {<PrivateRoute><NotFound/></PrivateRoute>}/>
-        </Routes>
-        </BrowserRouter>
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<Register />} />  {/* página inicial */}
+                <Route path="/home" element={<Home />} />
+                <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </HashRouter>
     )
 }
 export {RouterApp}
